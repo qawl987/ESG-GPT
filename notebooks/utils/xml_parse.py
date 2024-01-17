5768f241daf2d5615191a122b472791f12ff0882
qawl987 <shiny861216@gmail.com>
2024-01-18 03:54:47 +0800

    feat: change xml_parse code

--- notebooks/utils/xml_parse.py
+++ notebooks/utils/xml_parse.py
@@ -10,60 +10,53 @@ from concurrent.futures import ThreadPoolExecutor
 import concurrent
 
 paragraph = []
-
-def recur_element(element, paragraph):
-    # print(element.tag, element.text)
-    # if element.tag == 'H1' or element.tag == 'H2' or element.tag == 'H3' or element.tag == 'TOCI':
-    #     paragraph.append(element.text)
-    try:
+class XmlParse():
+    def __init__(self, nation, xml_path, csv_path, sentence_min_len) -> None:
+        self.NATION = nation
+        self.XML_PATH = xml_path
+        self.CSV_PATH = csv_path
+        self.sentence_min_len = sentence_min_len
+        pass
+    def _recur_element(self, element, paragraph):
         if element == None or element.text==None:
             for sub_child in element:
-                recur_element(sub_child, paragraph)
+                self._recur_element(sub_child, paragraph)
             return paragraph
-        if element.tag == 'P' and len(element.text.split()) > 5:
+        if element.tag == 'P' and len(element.text.split()) > self.sentence_min_len:
             paragraph.append(element.text)
-        elif element.tag == 'L':
-            text = ''
-            for list_element in element:
-                x = recur_element(list_element, paragraph)
-                text = text if(x == None) else text + x
-            paragraph.append(text)
-        elif element.tag == 'LI':
-            return element.text
         for sub_child in element:
-            recur_element(sub_child, paragraph)
-    except Exception as e:
-        print(f"An error occurred while recur_element {file}: {e}")
-    return paragraph
+            self._recur_element(sub_child, paragraph)
+        return paragraph
     
-def preprocess(x):
-    x = x.replace('\n', ' ')
-    x = re.sub(r'\’', '\'', x)
-    x = contractions.fix(x)
-    x = re.sub(r'[^\x00-\x7F]', '', x)
-    x = re.sub(r'[\x0C]', r"\n\n", x)
-    x = re.sub(r'[!\"#$%&\'()*\+,-.\/:;<=>?@\[\\\]^_`{|}~]', '', x)
-    x = x.replace('\t', ' ')
-    x = x.strip()
-    x = ' '.join(wordsegment.segment(x))
-    if len(x.split()) < 10:
-        return None
-    return x
+    def _postprocess(self, x: str):
+        x = x.replace('\n', ' ')
+        x = re.sub(r'\’', '\'', x)
+        x = contractions.fix(x)
+        x = re.sub(r'[^\x00-\x7F]', '', x)
+        x = re.sub(r'[\x0C]', r"\n\n", x)
+        x = re.sub(r'[!\"#$%&\'()*\+,-.\/:;<=>?@\[\\\]^_`{|}~]', '', x)
+        x = x.replace('\t', ' ')
+        x = x.strip()
+        x = ' '.join(wordsegment.segment(x))
+        if len(x.split()) < self.sentence_min_len:
+            return None
+        return x
 
 
-def parse_xml(file: str):
-    try:
-        file_name = file.strip('.pdf')
-        paragraph = []
-        tree = ET.parse(f'../taiwanxml/{file_name}.xml')
-        root = tree.getroot()
-        paragraph = recur_element(root, paragraph)
-        paragraph = map(preprocess, paragraph)
-        paragraph = [x for x in paragraph if x is not None]
-        paragraph = pd.DataFrame({'paragraph': paragraph})
-        paragraph.to_csv(f'../csv/taiwan_xml/{file_name}.csv', sep=',', index=False, encoding='utf-8')
-    except Exception as e:
-        print(f"An error occurred while processing {file}: {e}")
+    def parse_xml(self, file_list: list[str]):
+        for file in file_list:
+            try:
+                file_name = file.strip('.pdf')
+                paragraph = []
+                tree = ET.parse(f'{self.XML_PATH}/{self.NATION}/{file_name}.xml')
+                root = tree.getroot()
+                paragraph = self._recur_element(root, paragraph)
+                paragraph = map(self._postprocess, paragraph)
+                paragraph = [x for x in paragraph if x is not None]
+                paragraph = pd.DataFrame({'paragraph': paragraph})
+                paragraph.to_csv(f'{self.CSV_PATH}/{self.NATION}/{file_name}.csv', sep=',', index=False, encoding='utf-8')
+            except Exception as e:
+                print(f"An error occurred while processing {file}: {e}")
 
 def __main__():
     file_list = [ '4_esun_1_154.pdf', '10_compal_1_169.pdf',
